b22f144ea0b7cac8af740383e2dd583a32b999a7
Aftab Ansari <aftabansari78210@gmail.com>
2026-06-17 17:04:47 +0530

    updated tupleMethod.py

---
+++ lecture_3/list_&_tuples/tuple/tupleMethod.py
@@ -0,0 +1,4 @@
+tup = (2,1,3,1,5,6,3,7,3,3) # tuple values
+print(tup.index(3)) # index() returns the first position of a value; first 3 is at index 2
+
+print(tup.count(3)) # count() returns how many times a value appears; 3 appears 4 times
